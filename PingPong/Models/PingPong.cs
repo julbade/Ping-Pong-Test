3034741ee0410964e1dbc9cd0f859a937062dc3c
Julius Bade and Mark Mangahas <julius@>
2018-09-05 14:21:32 -0700

    fixed errors

--- PingPong/Models/PingPong.cs
+++ PingPong/Models/PingPong.cs
@@ -1,31 +1,44 @@
-namespace pingpong
+using System;
+using System.Collections.Generic;
+
+namespace PingPongProgram
 {
   public class PingPong
   {
-    public bool IsPingPong(int num)
+    public static List<string> isPingPong(int number)
     {
-      if (num % 3 == 0)
+      List<string> result = new List<string> {};
+      for (int i=1 ; i <= number; i++ )
       {
-        return true;
-      }
-      else
-      {
-        return false;
+        if (i % 15 == 0)
+        {
+          result.Add("ping-pong");
+          Console.WriteLine("ping-pong");
+        }
+        else if (i % 5 == 0)
+        {
+          result.Add("pong");
+          Console.WriteLine("pong");
+
+        }
+        else if (i % 3 == 0)
+        {
+          result.Add("ping");
+          Console.WriteLine("ping");
+        }
+        else
+        {
+          result.Add(i.ToString());
+          Console.WriteLine(i.ToString());
+        }
       }
 
-      // else
-      // {
-      //   return false;
-      // }
-    //   else if (num %= 5 == "Pong");
-    //   {
-    //     return true;
-    //   }
-    //   else if num %= 3 && 5 == "Ping-Pong";
-    //   {
-    //     return false;
-    //   }
-    //
+      return result;
+    }
+
+    public static void Main()
+    {
+      isPingPong(100);
     }
   }
 }
--- Pingpong.Tests/ModelTests/PingPongTests.cs
+++ Pingpong.Tests/ModelTests/PingPongTests.cs
@@ -1,17 +1,40 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using pingpong;
+using PingPongProgram;
+using System.Collections.Generic;
 
-namespace pingpong.Tests
+namespace PingPongProgram.Tests
 {
   [TestClass]
-  public class PingPongGeneratorTest
+  public class PingPongTest
   {
+    // [TestMethod]
+    // public void isPingPong_isThree_True()
+    // {
+    //   PingPong testPingPong = new PingPong();
+    //   Assert.AreEqual("Ping", testPingPong.isPingPong(3));
+    // }
+    //
+    // [TestMethod]
+    // public void isPingPong_isFive_True()
+    // {
+    //   PingPong testPingPong = new PingPong();
+    //   Assert.AreEqual("Pong", testPingPong.isPingPong(5));
+    // }
+    //
+    // [TestMethod]
+    // public void isPingPong_isFifteen_True()
+    // {
+    //   PingPong testPingPong = new PingPong();
+    //   Assert.AreEqual("Ping-Pong", testPingPong.isPingPong(15));
+    // }
+
     [TestMethod]
-    public void IsPingPong_FollowtheRules_True()
+    public void isPingPong_untilNumber_True()
     {
-      PingPongGenerator testPingPong = new PingPongGenerator();
-      List<string> testList = new List<string>{"1", "2", "ping"};
-      CollectionAssert.AreEqual(testList, testPingPong.IsPingPong(3));
+      PingPong testPingPong = new PingPong();
+      List<string> testList = new List<string> {"1", "2", "ping", "4", "pong", "ping", "7", "8", "ping", "pong", "11", "ping", "13", "14", "ping-pong", "16"};
+
+      CollectionAssert.AreEqual(testList, PingPong.isPingPong(16));
     }
   }
 }
